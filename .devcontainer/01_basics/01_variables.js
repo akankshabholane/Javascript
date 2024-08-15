21deb119123ea15c7b80ba98293da60bb1090966
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2024-08-15 19:45:47 +0000

    commit 01_variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+let name = "akanksha";
+var city = 'pune';
+adminName = "Anushka"; //without variabe also we can store data. but avoid this method 
+let House;//undefined
+
+/*
+prefer not to use var due to block scope and functional scope
+*/
+
+console.table([accountId, name, city, adminName,House]);
