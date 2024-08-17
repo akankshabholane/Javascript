38bbfb33c83ab9086248d049514bf821b9489af0
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2024-08-17 16:24:13 +0000

    incomplete

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1);//here JS automatically converted 2 into number
+// console.log("02" > 1);
+//this above method give an unpredictable result in somecases so make sure that you use variable of same datatype during comparision
+//---------
+
+console.log(null > 0); //=> false
+console.log(null == 0);//=>false
+console.log(null>= 0);//=>true ...reason is that equality checking == and comparisons > >= < <= operators work differently..comparisons convert null to a number, treating it as 0 thats why 1 and 3 has suc outcomes
+
+
+
