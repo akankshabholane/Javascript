956bd66d66c6d18b18d834a4ace96244c63c47dc
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2024-08-17 15:28:32 +0000

    conversion and operation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+//"33"=> 33;
+//"33abc"=> NaN; type of NaN is number
+//true => 1;false => 0;
+//1 => true; ; 0 => false;
+//""=> false;
+//"Akanksha"=> true;
+console.log(typeof NaN);
+
+// let number = 33;
+// let numToString = String(number);
+// console.log(numToString);
+// console.log(typeof numToString);
+
+// let number = "33abc"
+// let abc = Number(number)
+// console.log(abc);
+
+
+// let number = 33;
+// let numToBoolean = Boolean(number)
+// console.log(numToBoolean);//=> true
+
+
+// let number;
+// let numToBoolean = Boolean(number)
+// console.log(numToBoolean);//=>false;
+
+//------------------------------------Operators----------------------
+
+// let value = 3;
+// let negValue = -(value);
+// console.log(negValue);
+
+console.log("1" + 2);//12 
+console.log(1 + "2");//12
+
+console.log("1"+2 + 2);//122
+console.log(2+2 + "1");//41 
+//If the string is in front, all numbers after that are treated as strings; if the string is at the end, all the numbers before it are treated as numbers.only applicabe if there are more than 2 numbers of "1 " + 2=> gives 12 only
+console.log(+true);//type conversion happends when we add "+" sign here....=> 1 is output
+//console.log(true+);//error
+console.log("2" * 3);//6
+console.log("2" * "3");//6
+
+
+
+
+
+
+
