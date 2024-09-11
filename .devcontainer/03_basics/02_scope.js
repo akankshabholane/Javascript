a0b029e561ca2086de3427414fb0570f0ef278d6
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2024-09-11 12:32:53 +0000

    SCOPE

--- .devcontainer/03_basics/02_scope.js
+++ .devcontainer/03_basics/02_scope.js
@@ -1,9 +1,15 @@
-let a = 10
+
+
+
+if(true){
+    let a = 10
 const b = 20
-var c = 30
+var c = 30//it gets printed
 
-console.log(a);
-console.log(b);
+
+}
+//console.log(a);
+//console.log(b);
 console.log(c); 
-
-
+//global scope k values can be used in local scope but reverse is not possible
+//INTERVIEW:-browsers ka global scope or node se global scope is different
