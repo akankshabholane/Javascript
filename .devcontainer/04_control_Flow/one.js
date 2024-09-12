27b32d9d373e2cea8013858047cc6d56ac5ee299
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2024-09-12 05:00:01 +0000

    one

---
+++ .devcontainer/04_control_Flow/one.js
@@ -0,0 +1,9 @@
+const balance = 1000
+
+if(balance >900){
+    console.log("Huge Balance");
+}
+            //OR
+// called as implicit scope
+if(balance >900) console.log("Huge Balance");
+//if(balance >900) console.log("test1"),console.log("test"); dont write such type of code
