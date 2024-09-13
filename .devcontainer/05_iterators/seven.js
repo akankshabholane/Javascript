7e232a12b3b4701cb5e4d9026450893133690f10
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2024-09-13 15:10:54 +0000

    function

---
+++ .devcontainer/05_iterators/seven.js
@@ -0,0 +1,9 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNumers.map( (items)=> items + 10 )
+
+console.log(newNums);
+
+//chaining :- where we can use multiple function
+
+const myNum = 
