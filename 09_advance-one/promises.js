06fe49d18d8456f5541fdc9be90325a1617b94f4
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2025-07-15 18:03:57 +0530

    Update promises.js

--- 09_advance-one/promises.js
+++ 09_advance-one/promises.js
@@ -104,3 +104,13 @@ async function getAllUsers(){
 }
 
 getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=> console.log(error));
