9d06e180632c70fd93f5072a4437109c1bed6876
Akanksha Bholane <119569413+akankshabholane@users.noreply.github.com>
2025-07-17 19:49:53 +0000

    bind and static

---
+++ 10_classes_and_classes/staticprop.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){ // there will be scenario where you dont wnat to give the access of createid to every object that is instansiated by this class..to do so use sstatic keyword ye uss method or property ko access hone se rokhta hai
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
